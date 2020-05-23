cdda5621e891c1da702e7fee00466b63f9f25d41
Santosh Sivaraj <santosh@fossix.org>
2020-05-23 08:20:16 +0530

    Add a default user and also show bug history
    Signed-off-by: Santosh Sivaraj <santosh@fossix.org>

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 )
 
 type BugConfig struct {
-	URL    string `yaml:"url"`
-	APIKey string `yaml:"api_key"`
+	URL         string   `yaml:"url"`
+	APIKey      string   `yaml:"api_key"`
+	Users       []string `yaml:"user_list"`
+	DefaultUser string   `yaml:"default_user"`
 }
 
 func main() {
@@ -34,8 +36,9 @@ func main() {
 
 	bz, _ := itracker.NewTracker(itracker.BUGZILLA, conf.URL)
 	bz.SetAPIKey(conf.APIKey)
+	bz.SetTimeout(10)
 
-	user, err := bz.GetUser("user@example.com")
+	user, err := bz.GetUser(conf.DefaultUser)
 	if err != nil {
 		panic(err)
 	}
@@ -45,9 +48,45 @@ func main() {
 		panic(err)
 	}
 
+	show_history := false
 	for _, b := range bugs {
-		if b.Status == "OPEN" || b.Status == "ASSIGNED" {
-			fmt.Println(b.ID, b.Status, b.Summary)
+		if b.Status == "OPEN" || b.Status == "ASSIGNED" || b.Status == "NEEDINFO" {
+			if b.Status == "NEEDINFO" && b.Flags[0].Requestee == user.Email {
+				fmt.Println(b.Flags)
+			}
+			fmt.Println(b.ID, b.Summary)
+			fmt.Printf(" %-25s %-25s\n",
+				fmt.Sprintf("Status: %s", b.Status),
+				fmt.Sprintf("Created On: %s", b.CreationTime.Format("02/01/2006")))
+			if !show_history {
+				continue
+			}
+			if err = b.GetHistory(); err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			var prev_when string
+			var prev_who string
+			for _, h := range b.History {
+				when := h.When.Format("2006, January 02")
+				who := h.Who
+				if when != prev_when && who != prev_who {
+					fmt.Println("  On", when, "by", who)
+				}
+				prev_when = when
+				prev_who = who
+
+				for _, c := range h.Changes {
+					fmt.Printf("    %s: \n", c.FieldName)
+					if c.Added != "" {
+						fmt.Println("      + ", c.Added)
+					}
+					if c.Removed != "" {
+						fmt.Println("      - ", c.Removed)
+					}
+				}
+			}
 		}
 	}
 }
